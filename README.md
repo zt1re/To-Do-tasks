f13650bc8734bb9be30345c97ca49fc651fe3ffd
Zainab Tariq <91024751+zt1re@users.noreply.github.com>
2024-09-11 14:36:39 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# To-Do-tasks" 
+"# To-Do-tasks" 
---
+++ main.js
@@ -0,0 +1,42 @@
+const currentTasks = document.querySelector('#currentTasks');
+const completedTasks = document.querySelector('#completedTasks');
+const customPrompt = document.querySelector('#customPrompt');
+
+const submitTask = () => {
+    const taskLabel = userInput.value; 
+
+    if (taskLabel === "") {
+        alert("Please enter a task!"); 
+        return;
+    }
+
+    const newTask = document.createElement('div');
+    const newTaskcheckbox = document.createElement('input');
+    const newTaskLabel = document.createElement('span');
+
+    newTaskcheckbox.type = 'checkbox';
+    newTaskLabel.textContent = taskLabel;
+
+    newTaskcheckbox.addEventListener('click', () => {
+        completedTasks.appendChild(newTask);
+        currentTasks.removeChild(newTask);
+    });
+
+    newTask.appendChild(newTaskcheckbox);
+    newTask.appendChild(newTaskLabel);
+    currentTasks.appendChild(newTask);
+
+    customPrompt.classList.remove('active');
+
+};
+
+submit.addEventListener('click', submitTask);
+
+
+function showCustomPrompt() {
+    customPrompt.classList.add('active');
+}
+
+function removeCustomPrompt() {
+    customPrompt.classList.remove('active');
+}
